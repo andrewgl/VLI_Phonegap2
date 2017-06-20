bcaa8113d3c9a565da8fd4fd2d63dd82ad598e58
andrewwg <gleedois@gmail.com>
2017-06-20 22:28:48 +1000

    Cant quite get cordova to work...

---
+++ www/js/vliscripts.js
@@ -0,0 +1,193 @@
+var mainApp = angular.module('mainApp', ['ngRoute', 'ngResource']);
+
+mainApp.config(function ($routeProvider) {
+    $routeProvider
+        .when('/title', {
+            templateUrl: 'title.html',
+            controller: 'MainCtrl'
+        })
+        .when('/login', {
+            templateUrl: 'login.html',
+            controller: 'MainCtrl'
+        })
+        .when('/studentdetails', {
+            templateUrl: 'studentdetails.html',
+            controller: 'StudentCtrl'
+        })
+        .when('/test1', {
+            templateUrl: 'test1.html',
+            controller: 'Test1Ctrl'
+        })
+        .when('/test2', {
+            templateUrl: 'test2.html',
+            controller: 'Test2Ctrl'
+        })
+        .when('/review', {
+            templateUrl: 'review.html',
+            controller: 'ReviewCtrl'
+        })
+        .when('/end', {
+            templateUrl: 'end.html',
+            controller: 'MainCtrl'
+        })
+        .otherwise({
+            redirectTo: '/title',
+            controller: 'MainCtrl'
+        });
+});
+
+mainApp.controller('MainCtrl', function ($scope, $location) {
+    $scope.go = function (path) {
+        $location.path(path);
+    }
+});
+
+mainApp.factory('savedata', function () {
+
+    var allresults = {};
+    allresults["empty"]=true;
+
+    return {
+        saveResults: function (key, value) {
+            allresults[key] = value;
+        },
+
+        getResults: function () {
+            return allresults;
+        }
+    };
+
+});
+
+mainApp.controller('StudentCtrl', function ($scope, $location, savedata) {
+    $scope.go = function (path) {
+        savedata.saveResults("Details", angular.copy(this.results));
+        $location.path(path);
+    };
+
+    $scope.allresults = savedata.getResults();
+    $scope.results= {
+        "name": "",
+        "id": "",
+        "age": "",
+        "other": ""
+        }
+});
+
+mainApp.controller('Test1Ctrl', function ($scope, $location, savedata) {
+    $scope.go = function (path) {
+        savedata.saveResults("test1", angular.copy(this.results));
+        $location.path(path);
+    };
+    $scope.allresults = savedata.getResults();
+    $scope.results= {
+        "letter": {
+            "left": {
+                'fail': {
+                    '1': {id: 1, value: false},
+                    '2': {id: 2, value: false},
+                    '3': {id: 3, value: false},
+                    '4': {id: 4, value: false},
+                    '5': {id: 5, value: false}
+                },
+                'pass': {
+                    '6': {id: 6, value: false},
+                    '7': {id: 7, value: false},
+                    '8': {id: 8, value: false}
+                }
+            },
+            "right": {
+                'fail': {
+                    '1': {id: 1, value: false},
+                    '2': {id: 2, value: false},
+                    '3': {id: 3, value: false},
+                    '4': {id: 4, value: false},
+                    '5': {id: 5, value: false}
+                },
+                'pass': {
+                    '6': {id: 6, value: false},
+                    '7': {id: 7, value: false},
+                    '8': {id: 8, value: false}
+                }
+            }
+        },
+        "shape": {
+            "left": {
+                'fail': {
+                    '1': {id: 1, value: false},
+                    '2': {id: 2, value: false},
+                    '3': {id: 3, value: false},
+                    '4': {id: 4, value: false},
+                    '5': {id: 5, value: false}
+                },
+                'pass': {
+                    '6': {id: 6, value: false},
+                    '7': {id: 7, value: false},
+                    '8': {id: 8, value: false}
+                }
+            },
+            "right": {
+                'fail': {
+                    '1': {id: 1, value: false},
+                    '2': {id: 2, value: false},
+                    '3': {id: 3, value: false},
+                    '4': {id: 4, value: false},
+                    '5': {id: 5, value: false}
+                },
+                'pass': {
+                    '6': {id: 6, value: false},
+                    '7': {id: 7, value: false},
+                    '8': {id: 8, value: false}
+                }
+            }
+        }
+    }
+});
+
+mainApp.controller('Test2Ctrl', function ($scope, $location, savedata) {
+    $scope.go = function (path) {
+        savedata.saveResults("test2", angular.copy(this.results));
+        $location.path(path);
+    }
+    $scope.allresults = savedata.getResults();
+    $scope.results= {
+        "left": {
+            'fail': {
+                '1': {id: 1, value: false},
+                '2': {id: 2, value: false},
+                '3': {id: 3, value: false},
+                '4': {id: 4, value: false},
+                '5': {id: 5, value: false}
+            },
+            'pass': {
+                '6': {id: 6, value: false},
+                '7': {id: 7, value: false},
+                '8': {id: 8, value: false}
+            }
+        },
+        "right": {
+            'fail': {
+                '1': {id: 1, value: false},
+                '2': {id: 2, value: false},
+                '3': {id: 3, value: false},
+                '4': {id: 4, value: false},
+                '5': {id: 5, value: false}
+            },
+            'pass': {
+                '6': {id: 6, value: false},
+                '7': {id: 7, value: false},
+                '8': {id: 8, value: false}
+            }
+        }
+    }
+})
+
+mainApp.controller('ReviewCtrl', function ($scope, $location, savedata) {
+    $scope.go = function (path) {
+        // savedata.saveResults("test2", angular.copy(this.results));
+        $location.path(path);
+    }
+    $scope.allresults = savedata.getResults();
+
+});
+
